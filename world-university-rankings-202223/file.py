f01760304003d570c740cb0e048ae3859eb71eaf
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-07-04 18:38:48 +0500

    Python MySQL Database Connection using MySQL Connector

--- world-university-rankings-202223/file.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('/home/dell/Data/Introduction to data analyst and data science for begineers/SQL-for-beginners/world-university-rankings-202223/2023_QS_World_University_Rankings.csv')
-# print(df.head())
-denmark = df[df.location == 'Denmark']
-print(denmark)
